f55d5fb67b2a15a7a0684b80a981d28cff00d20d
abdousami02 <medsami246@gmail.com>
2022-01-11 15:48:14 +0100

    This the first Demo of Templet

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    
+    // $('.landing').height($(window).height() - $(".header .nav-bar").innerHeight())
+    // $(window).resize(function(){
+    //     $('.landing').height($(window).height() - $(".header .nav-bar").innerHeight())
+    // })
+
+
+    // trigger MixItup
+    $('#container').mixItUp();
+
+    // show the progress of skills
+    
+    // $('.test-skills .skills .content .progress').each(function(i){
+    //     let val = [];
+    //     val[i] = $(this).attr('data-progress')
+    //     $(this).find('> span').width(val[i]);
+    //     $(this).find('.value span').css('left', val[i])
+    //     $(this).find('.value span').text(val[i])
+    // })
+    
+})
